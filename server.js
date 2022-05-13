aa245a281af91a0e50a5bef51f9a35228539e109
Leonardorossato <leonardorossato21@gmail.com>
2022-05-12 22:01:36 -0300

    imports, porta e conexão com o banco

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+require('dotenv').config()
+const PORT = process.env.PORT
+
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.listen(PORT, ()=>{
+    console.log(`Server is runnin on the port : ${PORT}`)
+})

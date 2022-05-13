a91bf2300c710594bff408298e90a495ec915cf5
Leonardorossato <leonardorossato21@gmail.com>
2022-05-12 22:11:13 -0300

    dotnev para os segredos

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,16 @@
 const User = require('../models/User')
 const CryptoJs = require('crypto-js');
 const jwt = require('jsonwebtoken')
+require('dotenv').config()
+const JWT_SEC = process.env.JWT_SEC
+const PASS_SEC = process.env.PASS_SEC
 
 class AuthController{
     static register = async(req, res) => {
         const newUser = new User({
             username: req.body.username,
             email: req.body.email,
-            password: CryptoJs.AES.encrypt(req.body.password, process.env.PASS_SEC).toString(),
+            password: CryptoJs.AES.encrypt(req.body.password, PASS_SEC).toString(),
         });
         try {
             const savedUser = await newUser.save();
@@ -22,19 +25,18 @@ class AuthController{
             const user = await User.findOne({username: req.body.username});
             !user && res.status(401).json('Credentials Invalids')
     
-            const hasPassword = CryptoJs.AES.decrypt(user.password, process.env.PASS_SEC)
+            const hasPassword = CryptoJs.AES.decrypt(user.password,PASS_SEC)
             const OriginalPassword = hasPassword.toString(CryptoJs.enc.Utf8) 
             
             const accessToken = jwt.sign({
                 id: user.id, isAdmin: user.isAdmin
-            }, process.env.JWT_SEC, {expiresIn: '3d'})
+            }, JWT_SEC, {expiresIn: '3d'})
     
             OriginalPassword !== req.body.password && res.status(401).json('Password doesnt match!')
-            const {password, ...others} = user._doc;
     
-            res.status(200).json({...others, accessToken})
+            res.status(200).json({message: 'Ok', accessToken})
         }catch (err) {
-            res.status(500).json(err)
+            res.status(500).json({err : err.message})
         }
     }
 }
